b8710c2b7576187bd6cec9a9ceeed5fcf96a6905
axmand <815966508@qq.com>
2026-03-18 17:59:11 +0800

    rev

--- src/index.ts
+++ src/index.ts
@@ -4,37 +4,37 @@
  * - max mipmap level
  * - hash32a
  */
-export * from './util/uniqueID.ts'
-export * from './util/getMaxMipmapLevel.ts'
-export * from './util/hash32a.ts'
-export * from './util/align4Byte.ts'
+export * from './util/uniqueID.ts';
+export * from './util/getMaxMipmapLevel.ts';
+export * from './util/hash32a.ts';
+export * from './util/align4Byte.ts';
 
 /**
  * core/basic modules
  * context and compiler
  */
-export * from './res/Context.ts'
-export * from './res/Format.ts'
-export * from './res/Handle.ts'
-export * from './compile/Compiler.ts'
+export * from './res/Context.ts';
+export * from './res/Format.ts';
+export * from './res/Handle.ts';
+export * from './compile/Compiler.ts';
 
 /**
  * holder:
  * - compute holder
  * - render holder
  */
-export * from './holder/BaseHolder.ts'
-export * from './holder/ComputeHolder.ts'
-export * from './holder/RenderHolder.ts'
+export * from './holder/BaseHolder.ts';
+export * from './holder/ComputeHolder.ts';
+export * from './holder/RenderHolder.ts';
 
 /**
  * property:
  * - attribute
  * - uniforms
  */
-export * from './property/Properties.ts'
-export * from './property/dispatch/ComputeProperty.ts'
-export * from './property/dispatch/RenderProperty.ts'
+export * from './property/Properties.ts';
+export * from './property/dispatch/ComputeProperty.ts';
+export * from './property/dispatch/RenderProperty.ts';
 
 /**
  * cpu-gpu warp resource
@@ -45,25 +45,25 @@ export * from './property/dispatch/RenderProperty.ts'
  * - shader
  * - texture
  */
-export * from './res/buffer/BaseBuffer.ts'
-export * from './res/buffer/IndexedBuffer.ts'
-export * from './res/buffer/IndexedIndirectBuffer.ts'
-export * from './res/buffer/IndexedStorageBuffer.ts'
-export * from './res/buffer/IndirectBuffer.ts'
-export * from './res/buffer/Mapbuffer.ts'
-export * from './res/buffer/StorageBuffer.ts'
-export * from './res/buffer/UniformBuffer.ts'
-export * from './res/buffer/VertexBuffer.ts'
-export * from './res/attachment/ColorAttachment.ts'
-export * from './res/attachment/DepthStencilAttachment.ts'
-export * from './res/pipeline/ComputePipeline.ts'
-export * from './res/pipeline/RenderPipeline.ts'
-export * from './res/sampler/TextureSampler.ts'
-export * from './res/shader/ComputeShader.ts'
-export * from './res/shader/FragmentShader.ts'
-export * from './res/shader/VertexShader.ts'
-export * from './res/texture/SurfaceTexture2D.ts'
-export * from './res/texture/Texture2D.ts'
-export * from './res/texture/Texture2DArray.ts'
-export * from './res/texture/TextureStorage2D.ts'
-export * from './res/texture/TextureCube.ts'
+export * from './res/buffer/BaseBuffer.ts';
+export * from './res/buffer/IndexedBuffer.ts';
+export * from './res/buffer/IndexedIndirectBuffer.ts';
+export * from './res/buffer/IndexedStorageBuffer.ts';
+export * from './res/buffer/IndirectBuffer.ts';
+export * from './res/buffer/Mapbuffer.ts';
+export * from './res/buffer/StorageBuffer.ts';
+export * from './res/buffer/UniformBuffer.ts';
+export * from './res/buffer/VertexBuffer.ts';
+export * from './res/attachment/ColorAttachment.ts';
+export * from './res/attachment/DepthStencilAttachment.ts';
+export * from './res/pipeline/ComputePipeline.ts';
+export * from './res/pipeline/RenderPipeline.ts';
+export * from './res/sampler/TextureSampler.ts';
+export * from './res/shader/ComputeShader.ts';
+export * from './res/shader/FragmentShader.ts';
+export * from './res/shader/VertexShader.ts';
+export * from './res/texture/SurfaceTexture2D.ts';
+export * from './res/texture/Texture2D.ts';
+export * from './res/texture/Texture2DArray.ts';
+export * from './res/texture/TextureStorage2D.ts';
+export * from './res/texture/TextureCube.ts';
